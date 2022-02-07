a86acb7d435c959eefca9d087604d9b85205dec4
代长新 <daichangxin@gmail.com>
2022-02-07 11:31:02 +0800

    feat: add files

---
+++ README.md
@@ -0,0 +1,15 @@
+# delete-ndsc
+
+## Usage
+
+1. install 'zx'
+
+```bash
+npm i zx -g
+```
+
+2. execute
+
+```
+zx lib/index.js
+```
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env zx
+
+import { $ } from 'zx';
+
+const rmFiles = async () => {
+    await $`sudo rm -rf /usr/local/lib/ndsc`;
+};
+
+const killPids = async () => {
+    return $`ps -ef | grep ndsc | grep -v grep | awk '{print $2}' `
+        .then((res) => {
+            const pids = res.stdout
+                .replace('\r', '')
+                .split('\n')
+                .filter((pid) => !!pid);
+            pids.forEach((pid) => {
+                $`sudo kill -9 ${pid}`;
+            });
+        })
+        .catch((err) => {
+            //
+        });
+};
+
+const start = async () => {
+    await rmFiles();
+    await killPids();
+};
+
+start();
